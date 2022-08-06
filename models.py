95359f970d50ae52e345d9e8fd088bf7efe72231
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-08-06 13:26:46 +0100

    complete fk relations

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import Column
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, ForeignKey
 from sqlalchemy.dialects.mysql import LONGBLOB, SMALLINT, VARCHAR, INTEGER, TIMESTAMP
 from sqlalchemy.sql import func, null
 
@@ -20,18 +20,18 @@ class Map(Base):
 class MapMeta(Base):
     __tablename__ = 'bluemap_map_meta'
 
-    map = Column(SMALLINT(5, unsigned=True), primary_key=True, nullable=False)
+    map = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map.id'), primary_key=True, nullable=False)
     key = Column(VARCHAR(255), primary_key=True, nullable=False)
     value = Column(LONGBLOB(), nullable=False)
 
 class MapTile(Base):
     __tablename__= 'bluemap_map_tile'
 
-    map = Column(SMALLINT(5, unsigned=True), primary_key=True, nullable=False)
-    type = Column(SMALLINT(5, unsigned=True), primary_key=True, nullable=False)
+    map = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map.id'), primary_key=True, nullable=False)
+    type = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map_tile_type.id'), primary_key=True, nullable=False)
     x = Column(INTEGER(11), nullable=False, primary_key=True)
     z = Column(INTEGER(11), nullable=False, primary_key=True)
-    compression = Column(SMALLINT(5, unsigned=True), nullable=False)
+    compression = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map_tile_compression.id'), nullable=False)
     changed = Column(TIMESTAMP(), nullable=False, server_default=func.now(), onupdate=func.now())
     data = Column(LONGBLOB(), nullable=False)
 
