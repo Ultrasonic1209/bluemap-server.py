da7b01f23e4a3214ec336993618fc9add4ba8fff
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-08-06 14:54:19 +0100

    accuracy

--- models.py
+++ models.py
@@ -14,36 +14,36 @@ class StorageMeta(Base):
 class Map(Base):
     __tablename__ = 'bluemap_map'
 
-    id = Column(SMALLINT(5, unsigned=True), primary_key=True, nullable=False, autoincrement=True)
+    id = Column(SMALLINT(unsigned=True), primary_key=True, nullable=False, autoincrement=True)
     map_id = Column(VARCHAR(255), unique=True, nullable=False)
 
 class MapMeta(Base):
     __tablename__ = 'bluemap_map_meta'
 
-    map = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map.id'), primary_key=True, nullable=False)
+    map = Column(SMALLINT(unsigned=True), ForeignKey('bluemap_map.id'), primary_key=True, nullable=False)
     key = Column(VARCHAR(255), primary_key=True, nullable=False)
     value = Column(LONGBLOB(), nullable=False)
 
 class MapTile(Base):
     __tablename__= 'bluemap_map_tile'
 
-    map = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map.id'), primary_key=True, nullable=False)
-    type = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map_tile_type.id'), primary_key=True, nullable=False)
-    x = Column(INTEGER(11), nullable=False, primary_key=True)
-    z = Column(INTEGER(11), nullable=False, primary_key=True)
-    compression = Column(SMALLINT(5, unsigned=True), ForeignKey('bluemap_map_tile_compression.id'), nullable=False)
+    map = Column(SMALLINT(unsigned=True), ForeignKey('bluemap_map.id'), primary_key=True, nullable=False)
+    type = Column(SMALLINT(unsigned=True), ForeignKey('bluemap_map_tile_type.id'), primary_key=True, nullable=False)
+    x = Column(INTEGER(), nullable=False, primary_key=True)
+    z = Column(INTEGER(), nullable=False, primary_key=True)
+    compression = Column(SMALLINT(unsigned=True), ForeignKey('bluemap_map_tile_compression.id'), nullable=False)
     changed = Column(TIMESTAMP(), nullable=False, server_default=func.now(), server_onupdate=func.now())
     data = Column(LONGBLOB(), nullable=False)
 
 class MapTileCompression(Base):
     __tablename__ = 'bluemap_map_tile_compression'
 
-    id = Column(SMALLINT(5, unsigned=True), primary_key=True, nullable=False, autoincrement=True)
+    id = Column(SMALLINT(unsigned=True), primary_key=True, nullable=False, autoincrement=True)
     compression = Column(VARCHAR(255), nullable=False, unique=True)
 
 class MapTileType(Base):
     __tablename__ = 'bluemap_map_tile_type'
 
-    id = Column(SMALLINT(5, unsigned=True), primary_key=True, nullable=False, autoincrement=True)
+    id = Column(SMALLINT(unsigned=True), primary_key=True, nullable=False, autoincrement=True)
     type = Column(VARCHAR(255), nullable=False, unique=True)
 
